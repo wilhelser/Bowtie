ef7292de90db40b8c4279aa48c43e6efc499d3b4
Casey Milstead <casey@caseymilstead.com>
2017-12-21 14:24:28 -0600

    Added Vue to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require('path');
-const webpack = require('webpack');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier')
 
 const config = {
 	entry: {
@@ -25,6 +26,16 @@ const config = {
   },
 	module: {
 		rules: [
+			{
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+					loaders: {
+			      scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+			      sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+			    }
+        }
+      },
 			{
 				test: /\.scss$/,
 				use: ExtractTextPlugin.extract({
@@ -49,12 +60,16 @@ const config = {
 				],
 				loader: 'babel-loader',
 				options: {
-					presets: ['es2015']
+					presets: ['env']
 				}
 			}
 		],
 	},
 	plugins: [
+		new WebpackBuildNotifierPlugin({
+			sound: 'Funk',
+			successSound: 'Pop'
+		}),
 		new BrowserSyncPlugin({
 			host: 'localhost',
       port: 3000,
@@ -70,7 +85,7 @@ const config = {
 		})
 	],
 	devtool: 'source-map'
-};
+}
 
 if (process.env.NODE_ENV === 'production') {
 	config.plugins.push(
@@ -83,7 +98,7 @@ if (process.env.NODE_ENV === 'production') {
 			sourceMap: true,
 		}),
 		new OptimizeCssAssetsPlugin()
-	);
+	)
 }
 
-module.exports = config;
+module.exports = config
