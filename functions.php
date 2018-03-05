dd873f2c836f06ecc8e8b89d31fb1fb761c253c4
Casey Milstead <casey@caseymilstead.com>
2018-03-05 16:52:48 -0600

    add svg upload support

--- functions.php
+++ functions.php
@@ -180,6 +180,17 @@ remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
 
 
+/*
+ * Allow SVG Uploads
+ * 
+ */
+
+function bowtie_mime_types($mimes) {
+ $mimes['svg'] = 'image/svg+xml';
+ return $mimes;
+}
+add_filter('upload_mimes', 'bowtie_mime_types');
+
 /**
  *
  * Add ACF Options Page
